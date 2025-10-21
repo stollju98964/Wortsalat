95286a305c7220633a93d8d1c9f4e54c2caca4b5
Duerr <tim.duerr@siemens.com>
2025-10-21 16:58:26 +0200

    test

--- Start_Windows/game.c
+++ Start_Windows/game.c
@@ -10,6 +10,8 @@
 /* * Wörter aus der Wortliste zufällig horizontal oder vertikal platzieren
    * restliche Felder mit zufälligen Buchstaben füllen */
 
+   //test
+   
 // Creates the word salad by placing words randomly and filling empty spaces
 int createWordSalad(char salad[MAX_SEARCH_FIELD_LEN][MAX_SEARCH_FIELD_LEN], unsigned int searchFieldLen, const char words[][MAX_WORD_LEN], unsigned int wordCount)
 {

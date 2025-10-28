0449cb5b046da593767c1e1ba8ad1d4b4e95d558
Stoll <julian.stoll@siemens.com>
2025-10-28 13:50:20 +0100

    Überprüfung, ob alle Wörter eingefügt wurden hinzugefügt Wörter zählen und einlesen Game starten Wenn Fehler, Error ausgeben

--- Start_Windows/main.c
+++ Start_Windows/main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 {
     int exitCode = EXIT_SUCCESS;
 
-    // Check if the correct number of arguments is provided Test
+    // überprüfen ob die richtige Anzahl an Argumenten übergeben wurde
     if(argc != 2)
     {
         fprintf(stderr, "Usage: %s <path to file with search words>\n", argv[0]);
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     }
     else
     {
-        char words[MAX_NUMBER_OF_WORDS][MAX_WORD_LEN]; // Array to hold the words to be used in the game
+        char words[MAX_NUMBER_OF_WORDS][MAX_WORD_LEN]; // Array für die Suchwörter
         unsigned int wordCount = 0;
 
         FILE *file = fopen(argv[1], "r");
@@ -27,13 +27,13 @@ int main(int argc, char *argv[])
         if(file != NULL)
         {
             unsigned int placedWords = 0;
-            char wordSalad[MAX_SEARCH_FIELD_LEN][MAX_SEARCH_FIELD_LEN]; // 2D array to store the word salad
+            char wordSalad[MAX_SEARCH_FIELD_LEN][MAX_SEARCH_FIELD_LEN]; // 2D array um den Wortsalat zu speichern
 
-            // Read words from file and store in 'words' array
+            // Wörter zählen und einlesen
             wordCount = readWords(file, words, MAX_NUMBER_OF_WORDS);
             fclose(file);
 
-            // Create the word salad by placing words into grid
+            // Wörter in den Wortsalat einfügen
             placedWords = createWordSalad(wordSalad, SALAD_SIZE, words, wordCount);
 
             // TODO:
@@ -41,10 +41,28 @@ int main(int argc, char *argv[])
             // Start the game if successful
             // error message if some words couldn't be placed
 
+            if(wordCount == 0)
+            {
+                fprintf(stderr, "No words found in file %s.\n", argv[1]);
+                exitCode = EXIT_FAILURE;
+            }
+            else if(placedWords == wordCount)
+            {
+                printf("All %u words placed successfully. Starting game...\n", wordCount);
+
+                /* echten GUI-Start aufrufen (prüfe Namen in graphicalGame.h) */
+                startGraphicalGame(wordSalad, SALAD_SIZE, words, wordCount);
+            }
+            else
+            {
+                // Fehlermeldung, wenn nicht alle Wörter platziert werden konnten
+                fprintf(stderr, "Only %u of %u words could be placed. Aborting.\n", placedWords, wordCount);
+                exitCode = EXIT_FAILURE;
+            }
         }
         else
         {
-            // Print error message if file couldn't be opened
+            //Error ausgeben, wenn die Datei nicht geöffnet werden konnte
             fprintf(stderr, "Could not open file %s for reading ...\n", argv[1]);
             exitCode = EXIT_FAILURE;
         }
